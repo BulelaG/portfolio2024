89b1ca0f8c3636f3f859368c3fcc5b5308580f3d
Bulela <bulelagomoshe1@gmail.com>
2024-01-09 10:16:20 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#typed-text', {
+    strings: ["Hi, I am a Fullstack Developer","Hi, I am a Fullstack Developer",  "Hi, I am a Multimedia Designer", "Let's connect."],
+    typeSpeed: 40,
+    backSpeed: 10,
+    loop: true
+});
