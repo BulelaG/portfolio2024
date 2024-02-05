7bb310d67cea5990167f558c252a71bcf49b4ac0
Bulela <bulelagomoshe1@gmail.com>
2024-02-05 16:08:03 +0200

    typd text loop cancelled

--- script.js
+++ script.js
@@ -2,5 +2,13 @@ var typed = new Typed('#typed-text', {
     strings: ["Hi, I am a Fullstack Developer","Hi, I am a Fullstack Developer",  "Hi, I am a Multimedia Designer", "Let's connect."],
     typeSpeed: 40,
     backSpeed: 10,
-    loop: true
+    // loop: true
+});
+
+$(document).ready(function () {
+    $('.progress .progress-bar').css("width",
+        function () {
+            return $(this).attr("aria-valuenow") + "%";
+        }
+    )
 });
